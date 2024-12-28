acf190831d28d8a91663737672574ff42367d8c7
vmp78 <manhphuocvu03@gmail.com>
2024-12-28 23:08:32 +0700

    sua fe

--- script.js
+++ script.js
@@ -1,77 +1,3 @@
-// script.js
-const extractTitle = (data) => {
-  return (
-    data.poemName ||
-    data.test_title ||
-    data.animalName ||
-    data.blogName ||
-    data.song ||
-    data.title ||
-    data.newsName ||
-    data.tenchude ||
-    data.vietnamese_name ||
-    "No Title"
-  );
-};
-
-// Hàm để trích xuất nội dung
-const extractContent = (data) => {
-  const content = [
-    data.description ?? "",
-    data.meaning ?? "",
-    data.code ?? "",
-    data.lyrics ?? "",
-    data.noidung ?? "",
-    data.paragram ?? "",
-    data.content ?? "",
-    data.Content ?? "",
-  ]
-    .filter(Boolean) // Lọc các trường không phải là null, undefined hoặc chuỗi rỗng
-    .join("<br><br><br>"); // Nối các trường lại với nhau, mỗi trường cách nhau bằng <br>
-
-  return content;
-};
-
-// Hàm trích xuất tiêu đề phụ
-const extractSubTitle = (data) => {
-  const title =
-    (data.alt_name ?? "") +
-    " " +
-    (data.timeAgo ?? "") +
-    " " +
-    (data.date ?? "") +
-    " " +
-    (data.type ?? "") +
-    " " +
-    (data.author ?? "") +
-    " " +
-    ((data.tendieu ?? "") +
-      " " +
-      (data.tendemuc ?? "") +
-      " " +
-      (data.tenchuong ?? "")) +
-    " " +
-    (data["Poem Form"] ?? "") +
-    " " +
-    (data.artists ?? "");
-  return title;
-};
-
-// Hàm trích xuất các thông tin bổ sung
-const extractAdditional = (data) => {
-  const add = [
-    (data.classify ?? []).join(", "),
-    (data.categories ?? []).join(", "),
-    data.tags ?? "",
-    data.keywords ?? "",
-    data.category ?? "",
-    data.status ?? "",
-  ]
-    .filter(Boolean) // Lọc bỏ các giá trị rỗng
-    .join(", "); // Nối các trường lại bằng dấu phẩy
-  return add;
-};
-
 // Hàm thực hiện tìm kiếm
 async function fetchSearchResults(query) {
   try {
@@ -100,50 +26,16 @@ function displayResults(songs) {
   songs.forEach((song) => {
     const dataDetail = song._source || song; // Adjust for API structure
 
-    const title = extractTitle(dataDetail);
-    const content = extractContent(dataDetail).replace(/\n/g, "<br>");
-    const subTitle = extractSubTitle(dataDetail);
-    const add = extractAdditional(dataDetail);
-
-    const songCard = document.createElement("div");
-    songCard.className = "song-card";
-
-    // Tạo HTML cho thẻ <img> nếu có image
-    let imageHTML = "";
-    if (dataDetail.first_img) {
-      imageHTML = `
-        <div style="text-align: center">
-          <img 
-            src="${dataDetail.first_img}" 
-            alt="Description" 
-            style="width: 600px; height: auto;" 
-          />
-        </div>
-        <br>
-      `;
-    }
-
     // Tạo HTML cho thẻ song card
     songCard.innerHTML = `
-      <h3>${title}</h3>
-      <div style="margin-top: 0.5px;">
-        <p>${subTitle}</p>
-      </div>
+      <h3>${dataDetail.title}</h3>
       <div class="lyrics-section">
         <button class="toggle-lyrics">Details</button>
         <div class="content" style="display: none">
-          ${imageHTML}<br>
-          ${content}<br><br>
-          Tags: ${add}
+          ${dataDetail.content}<br>
         </div>
       </div>
-      <a href="${
-        dataDetail.link ||
-        dataDetail.Link ||
-        dataDetail.url ||
-        dataDetail.comments ||
-        "#"
-      }" target="_blank">Link</a>
+      <a href="${dataDetail.link || "#"}" target="_blank">Link</a>
     `;
 
     // Xử lý sự kiện toggle lyrics
